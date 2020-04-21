457386d8f47c70ed4bb592afa6d35a425b353c6d
sanch941 <sanch941@gmail.com>
2020-04-21 11:21:59 +0600

    Create Faq page

---
+++ components/faq/FaqItem.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="faq__item">
+        <div class="faq__question" @click="toggleCollapse">
+            <div class="faq__question-icon">
+                <img :src="windowsIcon" alt="" />
+            </div>
+            <div class="faq__question-text">
+                {{ item.question }}
+            </div>
+        </div>
+        <transition name="collapse">
+            <div v-show="isCollapseActive" class="faq__answer">
+                <div class="faq__answer-icon">
+                    <img :src="windowsIcon" alt="" />
+                </div>
+                <div class="faq__answer-text">
+                    {{ item.answer }}
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            isCollapseActive: false
+        };
+    },
+    computed: {
+        windowsIcon() {
+            return require('../../assets/images/svg/faq/windows.svg');
+        }
+    },
+    methods: {
+        toggleCollapse() {
+            this.isCollapseActive = !this.isCollapseActive;
+        }
+    }
+};
+</script>
--- pages/faq.vue
+++ pages/faq.vue
@@ -1,3 +1,28 @@
 <template>
-    <div class="relative"></div>
+    <div class="section faq">
+        <div class="container">
+            <div class="section__title md-none">FAQ</div>
+            <div class="faq__content">
+                <faq-item
+                    v-for="(item, index) in faq"
+                    :key="index"
+                    :item="item"
+                />
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import FaqItem from '../components/faq/FaqItem.vue';
+import faq from '~/assets/json/faq';
+
+export default {
+    components: { FaqItem },
+    data() {
+        return {
+            faq
+        };
+    }
+};
+</script>
